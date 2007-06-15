f8fe5424b3c7b59c93196a382d7ebc6bc63b06c4
Jesse Vincent <jesse@bestpractical.com>
2007-06-15 18:53:12 +0000

    Modernize and generalize Artur's RT::Authen::OpenId module

---
+++ lib/RT/Authen/OpenID.pm
@@ -0,0 +1,15 @@
+
+=head1 COPYRIGHT
+
+This extension is Copyright (C) 2005,2007 Best Practical Solutions, LLC.
+Portions of this extension are Copyright (C) 2006 Artur Bergman
+
+It is freely redistributable under the terms of version 2 of the GNU GPL.
+
+=cut
+
+package RT::Authen::OpenID;
+
+our $VERSION = '0.02';
+
+1;
